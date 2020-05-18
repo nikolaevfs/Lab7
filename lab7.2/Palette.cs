a5e87f609b4f3421747fb4f6c4d99e9b5cc32c08
UncleFedor527 <56272782+UncleFedor527@users.noreply.github.com>
2020-05-18 03:08:34 +0300

    метод hex правильно отображает RGB подсказку

--- lab7.2/Palette.cs
+++ lab7.2/Palette.cs
@@ -21,8 +21,29 @@ namespace lab7._2
         {
             ColorPanel.BackColor = Color.FromArgb(tb_red.Value, tb_green.Value, tb_blue.Value);
             Clipboard.Clear();
-            Clipboard.SetText($"#{tb_red.Value:X}{tb_green.Value:X}{tb_blue.Value:X}");
+            Clipboard.SetText("#" + hex(tb_red.Value) +
+                                    hex(tb_green.Value) +
+                                    hex(tb_blue.Value));
             toolTip.SetToolTip(ColorPanel, Clipboard.GetText());
+        }
+
+        private string hex(int value)
+        {
+            string hex = Convert.ToString(value,16);
+            if (hex.Length < 2)
+            {
+                hex = 0 + hex;
+            }
+
+            if (hex[0] >= 97 && hex[0] <= 122){
+                hex = Convert.ToString(hex[0]).ToUpper() + hex[1];
+            }
+
+            if (hex[1] >= 97 && hex[1] <= 122)
+            {
+                hex = hex[0]+ Convert.ToString(hex[1]).ToUpper();
+            }
+            return hex;
         }
 
         private void color_scroll(object sender, EventArgs e)
